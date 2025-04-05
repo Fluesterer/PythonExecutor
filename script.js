dd36d6df2f7f9bd87ab4ef2b71eb8a6b7c1aa1cf
Fluesterer <nico.friesen1bausa@gmail.com>
2025-04-05 19:02:55 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Placeholder für mögliche zukünftige Funktionalitäten
+document.addEventListener("DOMContentLoaded", function() {
+    // Dies könnte Funktionen hinzufügen, z.B. für das Scrollen, dynamische Ladeinhalte oder das Einbinden von Animationen
+});
